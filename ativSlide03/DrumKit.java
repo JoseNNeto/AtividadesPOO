1f7b016265a7b84426a67804f83273877fb3e9ee
José Neto <64179002+JoseNNeto@users.noreply.github.com>
2023-03-08 09:36:31 -0300

    Add files via upload

---
+++ ativSlide03/DrumKit.java
@@ -0,0 +1,12 @@
+public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat(){
+        System.out.println("Ding ding da-ding");
+    }
+
+    void playSnare(){
+        System.out.println("Bang bang ba-bang");
+    }
+}
---
+++ ativSlide03/Episode.java
@@ -0,0 +1,16 @@
+class Episode{
+    int seriesNumber;
+    int episodeNumber;
+
+    void skipIntro(){
+        System.out.println("Skipping intro...");
+    }
+
+    void skipToNext(){
+        System.out.println("Loading next episode...");
+    }
+
+    void play(){
+        System.out.println("Playing the episode...");
+    }
+}
