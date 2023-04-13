89f852db3411ad5145260483bfedade0819bdcea
José Neto <64179002+JoseNNeto@users.noreply.github.com>
2023-04-13 16:04:31 -0300

    Add files via upload
    Questão6

---
+++ Lista de Exercícios/Lista de Exercícios/src/Questao6.java
@@ -0,0 +1,16 @@
+public class Questao6 {
+    public static void main(String[] args) {
+        String resposta;
+
+        Arvore newTree = new Arvore();
+
+        newTree.setIdade(0);
+        newTree.setNome("Arvere");
+        newTree.setIdade_max(2);
+        newTree.setTamanho(5.0);
+
+        double taxa_cresc = 0.5;
+
+        newTree.crescer(taxa_cresc);
+    }
+}
